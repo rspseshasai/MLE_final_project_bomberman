0ec49332a34b1d71097d0045fcbe3651dc48b301
rspseshasai <rspseshasai@gmail.com>
2023-09-14 02:33:26 +0530

    Added basic QLearning code fot Task 1

--- agent_code/tpl_agent/callbacks.py
+++ agent_code/tpl_agent/callbacks.py
@@ -4,6 +4,7 @@ import random
 
 import numpy as np
 
+from model import QLearning
 
 ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
 
@@ -17,19 +18,17 @@ def setup(self):
     after this method. This separation allows you to share your trained agent
     with other students, without revealing your training code.
 
-    In this example, our model is a set of probabilities over actions
-    that are is independent of the game state.
+    Initialize your Q-learning model here and assign it to self.q_learning.
 
-    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    :param self: This object is passed to all callbacks, and you can set arbitrary values.
     """
     if self.train or not os.path.isfile("my-saved-model.pt"):
-        self.logger.info("Setting up model from scratch.")
-        weights = np.random.rand(len(ACTIONS))
-        self.model = weights / weights.sum()
+        self.logger.info("Setting up Q-learning model from scratch.")
+        self.q_learning = QLearning()  # Initialize your Q-learning model
     else:
-        self.logger.info("Loading model from saved state.")
+        self.logger.info("Loading Q-learning model from saved state.")
         with open("my-saved-model.pt", "rb") as file:
-            self.model = pickle.load(file)
+            self.q_learning = pickle.load(file)
 
 
 def act(self, game_state: dict) -> str:
@@ -41,15 +40,46 @@ def act(self, game_state: dict) -> str:
     :param game_state: The dictionary that describes everything on the board.
     :return: The action to take as a string.
     """
-    # todo Exploration vs exploitation
-    random_prob = .1
-    if self.train and random.random() < random_prob:
-        self.logger.debug("Choosing action purely at random.")
-        # 80%: walk in any direction. 10% wait. 10% bomb.
-        return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
+    # Check if there are revealed coins in the current game state
+    if 'coins' in game_state and game_state['coins']:
+        # If there are revealed coins, calculate the distance to each coin
+        coin_positions = np.array(game_state['coins'])
+        agent_position = np.array(game_state['self'][3])
+        distances_to_coins = np.linalg.norm(coin_positions - agent_position, axis=1)
 
-    self.logger.debug("Querying model for action.")
-    return np.random.choice(ACTIONS, p=self.model)
+        # Choose the action that moves the agent closer to the nearest coin using Q-learning
+        nearest_coin_index = np.argmin(distances_to_coins)
+        nearest_coin_position = coin_positions[nearest_coin_index]
+
+        # Determine the direction to move towards the nearest coin
+        x_diff = nearest_coin_position[0] - agent_position[0]
+        y_diff = nearest_coin_position[1] - agent_position[1]
+
+        if abs(x_diff) > abs(y_diff):
+            # Move horizontally
+            if x_diff > 0:
+                action = 'RIGHT'
+            else:
+                action = 'LEFT'
+        else:
+            # Move vertically
+            if y_diff > 0:
+                action = 'DOWN'
+            else:
+                action = 'UP'
+    else:
+        # If there are no revealed coins, use Q-learning to choose the action
+        state = state_to_features(game_state)
+        action = self.q_learning.get_action(state)
+
+        # Random exploration during training
+        random_prob = 0.1
+        if self.train and random.random() < random_prob:
+            self.logger.debug("Choosing action purely at random.")
+            # 80%: walk in any direction. 10% wait. 10% bomb.
+            return np.random.choice(ACTIONS, p=[0.2, 0.2, 0.2, 0.2, 0.1, 0.1])
+
+    return action
 
 
 def state_to_features(game_state: dict) -> np.array:
--- agent_code/tpl_agent/train.py
+++ agent_code/tpl_agent/train.py
@@ -1,10 +1,9 @@
-from collections import namedtuple, deque
-
+from collections import deque, namedtuple
 import pickle
 from typing import List
-
 import events as e
-from .callbacks import state_to_features
+from callbacks import state_to_features, ACTIONS
+from model import QLearning
 
 # This is only an example!
 Transition = namedtuple('Transition',
@@ -15,6 +14,7 @@ TRANSITION_HISTORY_SIZE = 3  # keep only ... last transitions
 RECORD_ENEMY_TRANSITIONS = 1.0  # record enemy transitions with probability ...
 
 # Events
+COIN_COLLECTED_EVENT = e.COIN_COLLECTED
 PLACEHOLDER_EVENT = "PLACEHOLDER"
 
 
@@ -28,6 +28,8 @@ def setup_training(self):
     """
     # Example: Setup an array that will note transition tuples
     # (s, a, r, s')
+    # Create an instance of QLearning
+    self.q_learning = QLearning(num_actions=len(ACTIONS))
     self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
 
 
@@ -50,12 +52,24 @@ def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_
     """
     self.logger.debug(f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}')
 
-    # Idea: Add your own events to hand out rewards
-    if ...:
-        events.append(PLACEHOLDER_EVENT)
+    # Get the state representation (you should implement state_to_features function)
+    state = state_to_features(old_game_state)
 
-    # state_to_features is defined in callbacks.py
-    self.transitions.append(Transition(state_to_features(old_game_state), self_action, state_to_features(new_game_state), reward_from_events(self, events)))
+    # Get the action chosen by Q-learning
+    action = self.q_learning.get_action(state)
+
+    # Provide rewards for coin collection
+    if COIN_COLLECTED_EVENT in events:
+        self.logger.info(f"Collected a coin in step {new_game_state['step']}")
+        events.append(PLACEHOLDER_EVENT)  # You can add a custom event for coin collection if needed
+
+    # Update Q-values based on the observed rewards and state transitions
+    reward = reward_from_events(self, events)
+    next_state = state_to_features(new_game_state)
+    self.q_learning.update_q_value(state, action, reward, next_state)
+
+    # Store the chosen action for the next step
+    self.next_action = ACTIONS[action]
 
 
 def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
@@ -72,7 +86,8 @@ def end_of_round(self, last_game_state: dict, last_action: str, events: List[str
     :param self: The same object that is passed to all of your callbacks.
     """
     self.logger.debug(f'Encountered event(s) {", ".join(map(repr, events))} in final step')
-    self.transitions.append(Transition(state_to_features(last_game_state), last_action, None, reward_from_events(self, events)))
+    self.transitions.append(
+        Transition(state_to_features(last_game_state), last_action, None, reward_from_events(self, events)))
 
     # Store the model
     with open("my-saved-model.pt", "wb") as file:
@@ -89,7 +104,7 @@ def reward_from_events(self, events: List[str]) -> int:
     game_rewards = {
         e.COIN_COLLECTED: 1,
         e.KILLED_OPPONENT: 5,
-        PLACEHOLDER_EVENT: -.1  # idea: the custom event is bad
+        PLACEHOLDER_EVENT: -0.1  # Idea: the custom event is bad
     }
     reward_sum = 0
     for event in events:
